105709eabf0826a1987180e4fdb14dcc6117322d
Ahnaf <ahnafx88@gmail.com>
2021-07-26 18:22:25 +0600

    crlf lf issue solved

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_invigilator';
 $plugin->release = '1.0.0';
-$plugin->version = 2021072313;
+$plugin->version = 2021072602;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_BETA;
